e7b5dddf9a53cf7ab1b8a1e38299d6af782afc02
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-05 16:03:49 +0200

    Init person parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  counter: number = 0
+  id: number
+  name: string
+  ssn: string 
+  nationality: string
+  language: string
+  //TODO: Should I implement familyRelations?
+  dateOfBirth: string
+  dateOfDeath: string
+
+  constructor() {
+    this.id = this.counter++
+    this.name = 'Nimi'
+    this.ssn = 'DD.MM.YYYY(tunniste)XXXX'
+    this.nationality = 'Kansalaisuus'
+    this.language = 'Äidinkieli'
+    this.dateOfBirth = 'Syntymäpäivä'
+    this.dateOfDeath = 'Kuolinpäivä'
+  }
+
 }

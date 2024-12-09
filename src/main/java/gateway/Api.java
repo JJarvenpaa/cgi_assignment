c393e080024b7705c715220cb9bb46057ca18726
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-09 15:06:37 +0200

    Import routing from gateway package

---
+++ src/main/java/gateway/Api.java
@@ -0,0 +1,17 @@
+package gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class Api {
+    /* TODO: First steps for CREATE method:
+    create a route for /api/newPerson
+    Initialize a new array suitable for JSON data
+    Populate new array with type Person using test data
+    */
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().build();
+    }
+}

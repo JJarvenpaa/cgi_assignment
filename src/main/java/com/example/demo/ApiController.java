69dccc8d323a5d4119bd4444f61868546a33c5a0
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-10 15:53:45 +0200

    READ method functionality that returns personsList as a test for now

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -5,17 +5,32 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class ApiController {
-    // TODO: First steps for CREATE method:
     
     ArrayList<Person> personsList = new ArrayList<Person>(); //Initialize a new array suitable for JSON data
+    //TODO: write personsList to a file with JSON syntax to emulate a database
 
+    /* TODO: Steps for READ all functionality (GET)
+     * use /api/persons route but with GET request
+     * Return personsList to here as a test
+     * 
+     */
+    //Method for getting all Persons
+    @GetMapping("/api/persons")
+    public ArrayList<Person> getPersons() {
+        return personsList;
+    }
+    
     //Method for creating new Person
     @PostMapping("/api/persons") //create a route for /api/persons
     Person addNewPerson(@RequestBody Person newPerson) {
+        //TODO: use the actual JSON data provided via frontend
         //Add new Person object here
+        //TODO: add id as incrementing counter
         newPerson.name = "testi";
         newPerson.ssn = "150796-111V";
         newPerson.address = "testikatu 1B7";

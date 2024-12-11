688bce5bed0f2b54163072be13c1f5af5a03d74e
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-11 16:01:07 +0200

    WIP DELETE request method with helper shell command

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +17,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class ApiController {
     
+    //TODO: Can we use other than public in class and methods?
     ArrayList<Person> personsList = new ArrayList<Person>(); //Initialize a new array suitable for JSON data
     //TODO: write personsList to a file with JSON syntax to emulate a database
     public Integer counter = -1; //We want to start ID incrementing at 0
 
     
     //Method for getting all Persons
-    @GetMapping("/api/persons") // use /api/persons route but with GET request
+    @GetMapping("/api/persons") // Create a route for /api/persons for GET requests
     public ArrayList<Person> getPersons() {
         //TODO: read Persons from a json file
         //TODO: return Persons as a json to frontend
@@ -29,7 +33,7 @@ public class ApiController {
     }
     
     //Method for creating new Person
-    @PostMapping("/api/persons") //create a route for /api/persons
+    @PostMapping("/api/persons") //create a route for /api/persons for POST requests
     Person addNewPerson(@RequestBody Person newPerson) {
         //TODO: use the actual JSON data provided via frontend
         //Add new Person object here
@@ -70,6 +74,25 @@ public class ApiController {
         // Return modifiedPerson
         return personToModify;
     }
+
+    //TODO: Method for removing Person
+    /**
+     * Find Person with DELETE request ID from personsList
+     * delete Person from personsList
+     * Return string with deletion confirmation and id
+
+     */
+
+     //TODO: fix http 405 error with DELETE request
+     // Maybe tomcat prevents it somehow?
+    // @DeleteMapping("api/persons/{id}") //Create route for deleting Person with DELETE request
+     @RequestMapping(value="/api/persons/{id}", method={RequestMethod.DELETE, RequestMethod.GET})
+     public void deletePerson(@PathVariable Integer id) {
+         
+        //personsList.remove(id);
+        //return "deleted Person with ID: " + id;
+     }
+
 
     
    

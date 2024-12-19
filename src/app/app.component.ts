4fae78ad7bcf4ee3a08d5e5639e2159eb44507a1
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-19 15:38:35 +0200

    Import Formsmodule to our component and Person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,28 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import {FormsModule} from '@angular/forms';
+import {Person} from './person'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-  <main>
-    <div class="container">
-      <button>Lisää uusi henkilö</button>
-      <!-- TODO: make person parameters as inputs here-->
-        {{ name }}
-    </div>
-  </main>
-  <router-outlet />
-  `,
-  //templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-/*
-TODO: use this in component constructor
-class Person {
-  id: number
-  name: string
-  ssn: string 
-  nationality: string
-  language: string
-  //TODO: Should I implement familyRelations?
-  dateOfBirth: string
-  dateOfDeath?: string
 
-}
-*/
 
 export class AppComponent {
-  counter: number = 0
-  id: number
-  name: string
-  ssn: string 
-  nationality: string
-  language: string
-  //TODO: Should I implement familyRelations?
-  dateOfBirth: string
-  dateOfDeath: string
-
-  constructor() {
-    this.id = this.counter++
-    this.name = 'Nimi'
-    this.ssn = 'DD.MM.YYYY(tunniste)XXXX'
-    this.nationality = 'Kansalaisuus'
-    this.language = 'Äidinkieli'
-    this.dateOfBirth = 'Syntymäpäivä'
-    this.dateOfDeath = 'Kuolinpäivä'
+  person = new Person(
+    '',
+    '',
+    '',
+    '',
+  )
+  
+ onSubmit() {
+    console.log(this.person)
   }
+  
   
 }
 

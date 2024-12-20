ddcdb013e1e454c3630e6d7e91c11d94e114c05f
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-20 17:25:20 +0200

    Add Person inputs to the form

--- src/app/person.ts
+++ src/app/person.ts
@@ -5,7 +5,7 @@ export class Person {
         public language: string,
         public dateOfBirth: string,
         public address?: string,
-        public ssn?: string,
+        public ssn?: string, //ssn = Social security number
         public dateOfDeath?: string,
     ) {}
   }

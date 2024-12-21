3b7b5ae34089667939cf2bfa53716bcabfcce7b1
JJarvenpaa <23655632+JJarvenpaa@users.noreply.github.com>
2024-12-21 14:03:27 +0200

    add READ functionality as fetch request to list all known Persons by name for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import {FormsModule} from '@angular/forms';
-import {Person} from './person'
+import { FormsModule } from '@angular/forms';
+import { Person } from './person'
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -15,23 +15,35 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({providedIn: 'root'})
 export class AppComponent {
   constructor(private http: HttpClient) {}
-  
+
   person = new Person(
     '',
     '',
     '',
     '',
   )
-  
- onSubmit() {
-    console.log(this.person)
-    this.sendRequest(this.person)
+
+  persons: any //TODO: try to type this 
+  ngOnInit() {
+    //TODO: Make this its own service and a reusable sendRequest method that only takes parameters that define the request type
+    this.fetchPersonsRequest()
   }
   
-  sendRequest(person: Person) {
+ onSubmit() {
+    this.addPersonRequest(this.person)
+    //TODO: make fetch async, so it happens after the new Person is added, not at the same time
+    this.fetchPersonsRequest()
+  }
+  
+  addPersonRequest(person: Person) {
+  //TODO: validate Person obj before sending request
   //Send a POST request to our backend with Person obj
   this.http.post('http://localhost:8080/api/persons', person).subscribe(response => {  console.log('Response from API: ', response);});
   
+  }
+
+  fetchPersonsRequest() {
+    this.http.get('http://localhost:8080/api/persons').subscribe(persons => { this.persons = persons })
   }
   
   

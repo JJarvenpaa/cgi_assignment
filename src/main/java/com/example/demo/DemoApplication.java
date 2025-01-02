5bde7ac984f6624aafdacc1f2e8f514f5b5e30be
VeiJari <nope>
2025-01-02 12:52:28 +0200

    Fix comparison that counts the smallest number count between numbers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		//TODO: create array logic here and move it away from here when it works
 		// So the assignment is to check between array numbers how many numbers fit between them the least, in other words, how many times can we increment the first number by 1 between them (until the number to compare with an offset of -1)
-		int[] test1 = {1, 10, 4};
+		int[] test1 = {1, 10, 4, 5};
 		//TODO: find out if the array size is always fixed to 2 (3 values)
 		//TODO: this comparison should check all values between each other so 1 should against 4 and 10, 4 should check against 1 and 10, 10 should check against 4 and 1 etc
 		int numberCountBetween = 0;	
@@ -20,42 +20,51 @@ public class DemoApplication {
 			if(i + 1 == test1.length) {break;}
 			
  			
-			int currentNumberCountBetween = 0;
 
 			
-			currentNumberCountBetween = compareNumbersToLength(i, test1);
+			numberCountBetween = compareNumbersToLength(i, test1, numberCountBetween);
 
-
-			//On first iteration we have to get the first value to compare later
-			if(i == 0) { 
-				numberCountBetween = currentNumberCountBetween; 
-			} 
-			else if(currentNumberCountBetween <= numberCountBetween && currentNumberCountBetween > 0) { 
-				numberCountBetween = currentNumberCountBetween; 
-			} 
 		}
 
 		System.out.println("vastaus on: " + numberCountBetween);
 
 	}
 
-	public static int compareNumbersToLength(int currentIndex, int[] array) {
-		int numberCountBetween = 0;
+	public static int compareNumbersToLength(int currentIndex, int[] array, int numberCountBetween) {
 		int startNumberIndex = currentIndex;
+		int currentNumberCountBetween = 0;
 		//TODO: extend logic to check if i > 0 (not first iteration) we have to compare to previous numbers also
 		while(currentIndex < array.length - 1) {
 
-			numberCountBetween = countNumbersBetween(startNumberIndex, currentIndex, array);
+			currentNumberCountBetween = countNumbersBetween(startNumberIndex, currentIndex, array);
+
+			if(currentIndex == 0) { 
+				numberCountBetween = currentNumberCountBetween; 
+			} 
+			
+			else if(currentNumberCountBetween <= numberCountBetween && currentNumberCountBetween > 0) { 
+				numberCountBetween = currentNumberCountBetween; 
+			} 
+
 			currentIndex++;
 		}
+
+		
 
 		return numberCountBetween;
 	}
 
 	public static int countNumbersBetween(int startNumberIndex, int currentIndex, int[] array) {
-		int numberCountBetween = 0; 
+		int numberCountBetween = 0;
 		int nextNumberToCompare = array[currentIndex + 1];
+		int test = 0;
+		//TODO: if not first iteration, get the count of previous numbers to compare
+		for(int x = startNumberIndex; x > 0; x--) {
+			test++;
+		}
+		
 
+		//Compares numbers next in array
 		if(array[startNumberIndex] < nextNumberToCompare) {
 			//incrementing happens if currentnumber < nextnumber, decreasing happens if other way around
 			for(int j = 0; array[startNumberIndex] + j < nextNumberToCompare - 1; j++) {

3c2a7b9a77820b58ff30db42d4a2d25cfd04fb7b
VeiJari <nope>
2025-01-04 13:06:28 +0200

    Cleanup code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,25 +12,24 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		//TODO: create array logic here and move it away from here when it works
 		// So the assignment is to check between array numbers how many numbers fit between them the least, in other words, how many times can we increment the first number by 1 between them (until the number to compare with an offset of -1)
-		int[] test1 = {6, 4, 4, 6};
-		//TODO: find out if the array size is always fixed to 2 (3 values)
-		//TODO: this comparison should check all values between each other so 1 should against 4 and 10, 4 should check against 1 and 10, 10 should check against 4 and 1 etc
+		int[] test1 = {1, 3, 5, 7};
 		int numberCountBetween = 0;	
+		
 		for (int i = 0; i < test1.length; i++) {
-			if(i + 1 == test1.length) {break;}
+			if(i + 1 == test1.length) break; // i + 1 to prevent array index out of bounds
 			
-			numberCountBetween = compareNumbersToLength(i, test1, numberCountBetween);
+			numberCountBetween = compareNumbersUntilLast(i, test1, numberCountBetween);
 
 		}
 
-		System.out.println("vastaus on: " + numberCountBetween);
+		System.out.println("Vastaus on: " + numberCountBetween);
 
 	}
 
-	public static int compareNumbersToLength(int currentIndex, int[] array, int numberCountBetween) {
+	public static int compareNumbersUntilLast(int currentIndex, int[] array, int numberCountBetween) {
 		int startNumberIndex = currentIndex;
 		int currentNumberCountBetween = 0;
-
+		// array length -1 to prevent array index out of bounds
 		while(currentIndex < array.length - 1) {
 
 			currentNumberCountBetween = countNumbersBetween(startNumberIndex, currentIndex, array);
@@ -57,8 +56,8 @@ public class DemoApplication {
 		int nextNumberToCompare = array[currentIndex + 1];
 		
 		//Compares numbers next in array
-		if(array[startNumberIndex] < nextNumberToCompare) {
-			//incrementing happens if starting number < nextnumber, decreasing happens if other way around
+		if(array[startNumberIndex] < nextNumberToCompare) { //incrementing happens if starting number < nextnumber, decreasing happens if other way around
+			
 			for(int i = 0; array[startNumberIndex] + i < nextNumberToCompare - 1; i++) {
 				numberCountBetween++;
 			}

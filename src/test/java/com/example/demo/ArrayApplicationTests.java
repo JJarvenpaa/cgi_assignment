312bf773a8637d6ae9d69d23eb5cba0b96cbb189
VeiJari <nope>
2025-01-06 12:17:31 +0200

    Add rest of the tests

--- src/test/java/com/example/demo/ArrayApplicationTests.java
+++ src/test/java/com/example/demo/ArrayApplicationTests.java
@@ -13,17 +13,35 @@ class ArrayApplicationTests {
 	private ArrayApplication arrayApp;
 
 	@Test
-	void oneLeastNumber() {
-		int[] testArray = {1, 4, 10, 15};
+	//Only the first number has least numbers between and 1 < 4
+	void oneLeastNumberSmaller() {
+		int[] testArray = {15, 1, 4, 10};
 		int answer = arrayApp.leastNumbersBetween(testArray);
 		assertEquals(2, answer);
 	}
 
 	@Test
+	//Only the first number has least numbers between that and 4 > 1
+	void oneLeastNumberBigger() {
+		int[] testArray = {4, 1, 10, 15};
+		int answer = arrayApp.leastNumbersBetween(testArray);
+		assertEquals(2, answer);
+	}
+
+	@Test
+	//2 and 4 have 1 number between
 	void sameNumbersWithLeast() {
 		int[] testArray = {2, 4, 2, 4};
 		int answer = arrayApp.leastNumbersBetween(testArray);
 		assertEquals(1, answer);
+	}
+
+	@Test
+	//Should be 0, no number fit between
+	void noNumbersBetween() {
+		int[] testArray = {2, 2, 2, 2};
+		int answer = arrayApp.leastNumbersBetween(testArray);
+		assertEquals(0, answer);
 	}
 
 
